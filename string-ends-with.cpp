438b01b3207055fa34dcb8ac170423dc3da24ef6
Igor Novitskii <drmnnml@gmail.com>
2025-05-02 11:35:52 +0300

    string ending with solution

--- string-ends-with.cpp
+++ string-ends-with.cpp
@@ -2,12 +2,24 @@
 #include <string>
 
 bool solution(std::string const &str, std::string const &ending) {
-  return true;
+  int i = static_cast<int>(str.length());
+  int j = static_cast<int>(ending.length());
+  if (i < j) {
+    return false;
+  }
+  while (j > 0) {
+    if (str[i - 1] != ending[j - 1]) {
+      break;
+    }
+    i--;
+    j--;
+  }
+  return j == 0;
 }
 
 int main(int argc, char *argv[]) {
-  std::string a = "abc";
-  std::string b = "b";
+  std::string a = "abcabc";
+  std::string b = "abc";
 
   std::cout << solution(a, b) << "\n";
 }
