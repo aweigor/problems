6f8fd10f36889f8cb404249b592cae3106bc8533
Igor Novitskii <drmnnml@gmail.com>
2025-05-05 23:22:10 +0300

    codewars-square-digits

---
+++ strings.hpp
@@ -0,0 +1,15 @@
+//
+//  srtings.hpp
+//  algorithms
+//
+//  Created by Igor on 05.05.2025.
+//
+
+#ifndef strings_hpp
+#define strings_hpp
+
+#include <stdio.h>
+
+#endif /* strings_hpp */
+
+int square_digits(int num);
