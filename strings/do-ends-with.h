f1f35740f0103983ebcf9c4eb2668dfe126e328f
Igor Novitskii <drmnnml@gmail.com>
2025-10-26 11:21:51 +0300

    feat: add string ends with

---
+++ strings/do-ends-with.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+bool solution(std::string const &str, std::string const &ending) {
+  int i = static_cast<int>(str.length());
+  int j = static_cast<int>(ending.length());
+  if (i < j) {
+    return false;
+  }
+  while (j > 0) {
+    if (str[i - 1] != ending[j - 1]) {
+      break;
+    }
+    i--;
+    j--;
+  }
+  return j == 0;
+}
+
+int main(int argc, char *argv[]) {
+  std::string a = "abcabc";
+  std::string b = "abc";
+
+  std::cout << solution(a, b) << "\n";
+}
