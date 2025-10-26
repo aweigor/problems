bc955b8edd1ff62496c8acafc242aeb887f071b9
Igor Novitskii <drmnnml@gmail.com>
2025-10-26 11:22:39 +0300

    feat: add string spin words

---
+++ strings/spin-words.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+std::string spinWords(const std::string &str)
+{
+    std::stringstream ss(str);
+    std::string result;
+    std::string buff;
+    while (ss >> buff) {
+        if (buff.size() >= 5) {
+            std::reverse(buff.begin(), buff.end());
+        }
+        result += buff + ' ';
+    }
+    result.pop_back();
+    return result;
+}
