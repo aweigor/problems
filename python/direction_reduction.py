167a3b94492c387103e0ee65c0b6dfc66c9ee516
aweigor <drmnnml@gmail.com>
2026-01-18 06:56:54 +0300

    direction reduction first impl

--- python/direction_reduction.py
+++ python/direction_reduction.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # https://www.codewars.com/kata/550f22f4d758534c1100025a
 #
 # { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" } -> { "WEST" }
@@ -11,3 +13,62 @@
 # while out != reduc(out):
 #   out = reduc(out)
 # return out
+
+directionsMap = { 'N': 0, 'S': 1, 'E': 2, 'W': 3}
+
+def reduc(direc):
+    if not direc:
+        return ''
+
+    first = direc[0]
+
+    is_longitude = first in ("N", "S")
+
+    i = 0
+    count = 0
+    out = ''
+    direction_changed = False
+
+    while i < len(direc):
+        while i < len(direc) and (direc[i] in ("N", "S")) == is_longitude:
+            if is_longitude:
+                sign = -1 if direc[i] == "N" else 1
+            else:
+                sign = -1 if direc[i] == "W" else 1
+            count += sign
+            i += 1
+
+        if is_longitude:
+            if count < 0:
+                out += ''.join(["N"] * abs(count))
+            else:
+                out += ''.join(["S"] * count)
+        else:
+            if count < 0:
+                out += ''.join(["W"] * abs(count))
+            else:
+                out += ''.join(["E"] * count)
+
+        count = 0
+
+        if i < len(direc):
+            direction_changed = True
+            is_longitude = not is_longitude
+
+    if not direction_changed:
+        return out
+
+    reduced_out = reduc(list(out))
+    while out != reduced_out:
+        out = reduced_out
+        reduced_out = reduc(list(out))
+
+    return out
+
+def main():
+    res = reduc(["N", "S", "S", "E", "W", "N", "W"])
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
